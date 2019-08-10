8f6bdc934f35638670d2253df01a669f5f2780d7
Sulaymon333 <tjsulaymon@gmail.com>
2019-08-10 12:58:58 +0300

    gatsby css module, sticky footer, layout styling, nav-item styling - hover & active styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,16 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../styles/style.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={layoutStyles.container}>
+      {/* sticky footer */}
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   )

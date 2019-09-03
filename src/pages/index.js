9fbf05426fdf3c634233705f95b3bf8aa836cce8
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-04 01:14:54 +0300

    link added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
-  <div>
-    <h1 style={{ color: `purple` }}>Hello Gatsby!</h1>
+  <div style={{ color: `purple` }}>
+    <Link to="/contact">Contact</Link>
+    <Header headerText="About Gatsby" />
     <p>what a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>

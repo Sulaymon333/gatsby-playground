9a2721971c380fa807e21c0ac47db3b46fb7a046
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-04 00:49:19 +0300

    paragraph and image added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div>
+    <h1 style={{ color: `purple` }}>Hello Gatsby!</h1>
+    <p>what a world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 )

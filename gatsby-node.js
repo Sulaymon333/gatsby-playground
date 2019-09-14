87000cd8b2335be15fd2bc1502b068d2ac3a4159
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-14 12:13:06 +0300

    create slug using the file name

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,16 @@
-exports.onCreateNode = ({ node }) => {
+// exports.onCreateNode = ({ node }) => {
+//   console.log(node.internal.type)
+// }
+
+// exports.onCreateNode = ({ node }) => {
+//   if (node.internal.type === `MarkdownRemark`) {
+//     console.log(node.internal.type)
+//   }
+// }
+
+exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(node.internal.type)
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
   }
 }

6ab65333a1980dfe434434b4da8da1d75aabeffd
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-14 12:28:24 +0300

    create slug field using createNodeField

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,43 @@
+// log all the nodes available
 // exports.onCreateNode = ({ node }) => {
 //   console.log(node.internal.type)
 // }
 
+// filter by only MarkdownRemark files
 // exports.onCreateNode = ({ node }) => {
 //   if (node.internal.type === `MarkdownRemark`) {
 //     console.log(node.internal.type)
 //   }
 // }
 
-exports.onCreateNode = ({ node, getNode }) => {
+// create slug using file name
+// exports.onCreateNode = ({ node, getNode }) => {
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const fileNode = getNode(node.parent)
+//     console.log(`\n`, fileNode.relativePath)
+//   }
+// }
+
+// create slug name using createFilePath function from gatsby-source-filesystem
+// const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// exports.onCreateNode = ({ node, getNode }) => {
+//   if (node.internal.type === `MarkdownRemark`) {
+//     console.log(createFilePath({ node, getNode, basePath: `pages` }))
+//   }
+// }
+
+// using createNodeField function to create additional fields on nodes created by other plugins
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const fileNode = getNode(node.parent)
-    console.log(`\n`, fileNode.relativePath)
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
 }
